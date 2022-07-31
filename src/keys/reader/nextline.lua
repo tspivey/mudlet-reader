bf63af88ea6d4e8468e78a2c7d7c509f9bdae899
Tyler Spivey <tspivey@pcdesk.net>
2022-07-31 06:31:06 -0700

    Interrupt when reading lines

--- src/keys/reader/nextline.lua
+++ src/keys/reader/nextline.lua
@@ -3,6 +3,8 @@ reader.line = reader.line + 1
 if reader.line > getLineCount() - 1 then
 	reader.say("Bottom", true)
 	reader.line = getLineCount() - 1
+else
+	reader.stop()
 end
 local line = getLines(reader.line, reader.line + 1)[1]
 reader.sayline(line)
--- src/keys/reader/prevline.lua
+++ src/keys/reader/prevline.lua
@@ -2,6 +2,8 @@ reader.line = reader.line - 1
 if reader.line < 0 then
 	reader.say("Top", true)
 	reader.line = 0
+else
+	reader.stop()
 end
 local line = getLines(reader.line, reader.line + 1)[1]
 reader.sayline(line)
